ce1d14832cc5e2df83522f28855ec7a57ee1ab1b
unknown <dilorenzo@itis-molinari.eu>
2025-01-16 12:25:09 +0100

    mappa funzionante

--- src/components/map.js
+++ src/components/map.js
@@ -1,46 +1,47 @@
+
+
 export const generateMap = () => {
-    let places = [
-       {
-        //template del posto
-       }
-    ];
- 
+   let places = [
+      {
+         name: "Piazza del Duomo",
+         coords: [45.4639102, 9.1906426]
+      }
+   ];
+
+   
+   const zoom = 12;
+   const maxZoom = 19;
+   let map; 
+
+   return {
     
-    const zoom = 12;
-    const maxZoom = 19;
-    let map; 
- 
-    return {
-     
-       build: () => {
-          map = L.map("map").setView(places[places.length-1].coords, zoom);
-       },
- 
-       //render
-       render: () => {
-          
-          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-             maxZoom: maxZoom,
-             attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          }).addTo(map);
- 
-          places.forEach((place) => {
-             const marker = L.marker(place.coords).addTo(map);
-             marker.bindPopup(`<b>${place.name}<br>data: ${place.dataInc}<br>ora: ${place.oraInc}<br>feriti: ${place.feritiInc}<br>morti: ${place.mortiInc}</b>`);
-          });
-       },
- 
- 
-       addPlace: (place) => {
-          places.push(place);
-          console.log(places)
-       },
-       getPlaces: ()=>{
-          return places;
-       },
-       addAllPlaces: (p)=>{
-          places = p;
-       }
-    };
- };
- 
+      build: () => {
+         map = L.map("map").setView(places[0].coords, zoom);
+      },
+
+    
+      render: () => {
+         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: maxZoom,
+            attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+         }).addTo(map);
+
+         places.forEach((place) => {
+            const marker = L.marker(place.coords).addTo(map);
+            marker.bindPopup(`<b>${place.name}</b>`);
+         });
+      },
+
+
+      addPlace: (place) => {
+         places.push(place);
+         console.log(places)
+      },
+      getPlaces: ()=>{
+         return places;
+      },
+      addAllPlaces: (p)=>{
+         places = p;
+      }
+   };
+}; 
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { generateMap } from "/5C_BrambillaBianchinDiDonna_POI/src/components/map.js";
+import { createLogin } from "/5C_BrambillaBianchinDiDonna_POI/src/components/login.js";
+import { fetchComponent } from "/5C_BrambillaBianchinDiDonna_POI/src/components/fetch.js";
+import { createTable } from "/5C_BrambillaBianchinDiDonna_POI/src/components/table.js";
+import { createNavigator } from "/5C_BrambillaBianchinDiDonna_POI/src/components/navigator.js";
+console.log("osafj")
+let map = generateMap();
+map.build()
+map.render();
