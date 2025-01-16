ff5bf1195710c39a910b447f65f708aa3aff55e9
unknown <dilorenzo@itis-molinari.eu>
2025-01-16 12:50:34 +0100

    mappa - spa

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import { generateMap } from "/5C_BrambillaBianchinDiDonna_POI/src/components/map.js";
 import { createLogin } from "/5C_BrambillaBianchinDiDonna_POI/src/components/login.js";
 import { fetchComponent } from "/5C_BrambillaBianchinDiDonna_POI/src/components/fetch.js";
@@ -7,3 +8,15 @@ console.log("osafj")
 let map = generateMap();
 map.build()
 map.render();
+let loginDiv = document.querySelector("#login")
+
+function fhide(parentElement){
+    parentElement.classList.add("hide")
+    parentElement.classList.remove("show")
+}
+function fshow(parentElement){
+    parentElement.classList.add("show")
+    parentElement.classList.remove("hide")
+}
+fhide(loginDiv)
+
