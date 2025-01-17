7a92843c06fc93c0bcd22d4f0a834aa444e54281
unknown <dilorenzo@itis-molinari.eu>
2025-01-17 09:43:55 +0100

    sjfaafdnf

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 
-import { generateMap } from "/src/components/map.js";
-import { createLogin } from "/src/components/login.js";
-import { fetchComponent } from "/src/components/fetch.js";
-import { createTable } from "/src/components/table.js";
-import { createNavigator } from "/src/components/navigator.js";
+import { generateMap } from "/5C_BrambillaBianchinDiDonna_POI/src/components/map.js";
+import { createLogin } from "/5C_BrambillaBianchinDiDonna_POI/src/components/login.js";
+import { fetchComponent } from "/5C_BrambillaBianchinDiDonna_POI/src/components/fetch.js";
+import { createTable } from "/5C_BrambillaBianchinDiDonna_POI/src/components/table.js";
+import { createNavigator } from "/5C_BrambillaBianchinDiDonna_POI/src/components/navigator.js";
+
 console.log("osafj")
 let map = generateMap(document.getElementById("map"));
-map.build()
+map.build();
 map.render();
 const nav = createNavigator(document.getElementById("contenitore"));
+
+
