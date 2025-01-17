6e92952d003b73a2d9dfa06fc9842ab08c7a3b06
unknown <dilorenzo@itis-molinari.eu>
2025-01-17 10:46:17 +0100

    modale per ogni posto

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { createLogin } from "/5C_BrambillaBianchinDiDonna_POI/src/components/log
 import { fetchComponent } from "/5C_BrambillaBianchinDiDonna_POI/src/components/fetch.js";
 import { createTable } from "/5C_BrambillaBianchinDiDonna_POI/src/components/table.js";
 import { createNavigator } from "/5C_BrambillaBianchinDiDonna_POI/src/components/navigator.js";
-
+const alpiCarniche = document.querySelector("#alpiCarniche")
 console.log("osafj")
 let map = generateMap(document.getElementById("map"));
 map.build();
 map.render();
 const nav = createNavigator(document.getElementById("contenitore"));
-
+alpiCarniche.onclick=()=>{
+   
+}
 
