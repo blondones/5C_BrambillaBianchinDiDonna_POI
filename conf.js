e7440ada4280156ed53fa0cfee33041cad9b10b7
Brambilla05 <brambillagianluca@itis-molinari.eu>
2025-01-23 11:22:47 +0100

    conf

---
+++ conf.js
@@ -0,0 +1,3 @@
+export const cacheToken= "01aeec9c-9533-46c4-85e1-dfc449335c90";
+export const set = "https://ws.cipiaceinfo.it/cache/set";
+export const get = "https://ws.cipiaceinfo.it/cache/get";
